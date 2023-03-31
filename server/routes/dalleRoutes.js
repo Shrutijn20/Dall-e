ab9d49dbf56c446180dbe0e811e83f1664eb7b6c
Shruti Jain <91418428+Shrutijn20@users.noreply.github.com>
2023-03-31 21:00:56 +0530

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from "dotenv";
 import { Configuration,OpenAIApi } from 'openai';
 
-import Post from '../mongodb/models/post.js';
+
 
 dotenv.config();
 
